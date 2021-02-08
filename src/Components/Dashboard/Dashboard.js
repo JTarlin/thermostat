7b6e27caf257210e10053e411e35e33a4684979e
Jackson Tarlin <jacksontarlin@gmail.com>
2021-02-07 16:34:53 -0800

    rendered units can be clicked to select them

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,6 +2,9 @@
 import {useState} from "react";
 import Axios from "axios";
 
+//component imports
+import UnitList from "../UnitList/UnitList";
+
 //constant urls for api calls
 const registerURL = "https://api-staging.paritygo.com/sensors/api/thermostat/register/";
 
@@ -9,21 +12,21 @@ export default function Dashboard() {
 
     //state tracks the info of each housing unit
     const [units, setUnits] = useState(localStorage.getItem("units"));
+    //state tracks currently selected housing unit/thermostat
+    const [selectedUnit, setSelectedUnit] = useState(null);
 
     const register = ()=>{
         Axios.post(registerURL)
             .then(function (response) {
                 if(units){
                     //if we have already set units then add new thermostat to registry
-                    console.log("running the units branch of register")
-                    let unitObj = units;
-                    console.log(unitObj);
-                    unitObj.unitArr.push({id: response.data.uid_hash, state: response.data.state});
-                    setUnits(unitObj);
-                    localStorage.setItem('units', JSON.stringify(unitObj));
+                    let newUnitArr = units.unitArr; //new reference to array so that state update causes re-render
+                    newUnitArr.push({id: response.data.uid_hash, state: response.data.state});
+                    const newUnitObj = {unitArr: newUnitArr};
+                    setUnits(newUnitObj);
+                    localStorage.setItem('units', JSON.stringify(newUnitObj));
                 } else {
                     // we have not set units then this is the first thermostat
-                    console.log("running the no units branch of register");
                     const newUnitObj = {unitArr: [{id: response.data.uid_hash, state: response.data.state}]};
                     setUnits(newUnitObj);
                     localStorage.setItem('units', JSON.stringify(newUnitObj));
@@ -34,11 +37,16 @@ export default function Dashboard() {
             });
     }
 
+    const select = (unit)=>{
+        console.log(unit.id);
+        // setSelectedUnit(unit);
+    }
+
     if(units){
         return (
             <div className="dashboard">
-                <div className="units-list"></div>
                 <div className="dash__register--big" onClick={register}>Register New Thermostat</div>
+                <UnitList units={units.unitArr} key={units.unitArr} select={select}/>
             </div>
         )
     } else {
--- src/Components/UnitList/UnitList.js
+++ src/Components/UnitList/UnitList.js
@@ -1,1 +1,16 @@
-export default 
+export default function UnitList(props) {
+
+    console.log(props.units);
+    const units = props.units;
+    const select = props.select;
+
+    const unitStack = units.map(unit=>{
+        return (<div key={unit.id} onClick={()=>select(unit)}>{unit.id}</div>)
+    });
+
+    return (
+        <div>
+            {unitStack}
+        </div>
+    )
+}
